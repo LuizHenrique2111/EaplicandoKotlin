1ff4c31fa56bfda312165812dd35a68855451e4e
Luiz Henrique <luizbtc13@gmail.com>
2023-03-17 22:42:27 -0300

    Print - Println

---
+++ src/main/kotlin/PrintEPrintln.kt
@@ -0,0 +1,20 @@
+// A linguagem de programação Kotlin foi adotada pelo Google como a linguagem
+// padrão para a construção de aplicativos android, é uma linguagem moderna e
+// simplificada
+
+
+// Assim como Java a linguagem Kotlin é orientada por objetos e suas declaração
+// são feitas dentro do escopo( {} ).
+fun main(args: Array<String>) {
+
+    // Assim como todas as linguagens ela tem o print, que imprime aquilo que é
+    // inserido dentro dos parenteses, conforme codigo abaixo.
+    println("Hello World!")
+    println("Eu aqui de novo")
+
+    // Nesse exemplo utlizamos o println, que faz com que após a leitura dos dados
+    // pula uma linha, utilizando apenas o print, o programa continuaria na mesma linha
+    print("Hello World!")
+    print("Eu aqui de novo")
+
+}
