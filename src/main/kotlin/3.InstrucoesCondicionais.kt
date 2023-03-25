a899dbf2924e583dd31f5559bf8cdccb2961b1b8
Luiz Henrique <luizbtc13@gmail.com>
2023-03-25 18:05:25 -0300

    Conditional Structures 2

--- src/main/kotlin/3.InstrucoesCondicionais.kt
+++ src/main/kotlin/3.InstrucoesCondicionais.kt
@@ -7,7 +7,7 @@ fun main() {
 
     // O if e if else funiona como uma bifurcação, onde você vai seguir de acordo com a condição
     // criada dentro dos parenteses do if
-    if (age > 18) {
+   if (age > 18) {
         println("Você já pode beber bebida alcoolica.\nSua idade: $age")
     } else {
         println("Você ainda não tem idade para beber bebida alcoolica. \nSua idade: $age")
@@ -15,12 +15,22 @@ fun main() {
 
     // Uma estrutura condicional do kotlin é o when, ele funciona de maneira semelhante ao switch em
     // outras linguagens e pode substituir o if else, em diversos pontos
-    var sexo = "M"
-
-    when(sexo) {
-        "M" -> println("Masculino")
-        "F" -> println("Feminino")
-        else -> println("Valores inseridos incorretamente")
+    when {
+        age > 18 -> {
+            println("Você já pode beber bebida alcoolica.\nSua idade: $age")
+        }
+        else -> {
+            println("Você ainda não tem idade para beber bebida alcoolica. \nSua idade: $age")
+        }
     }
 
+    // Tem também o for, que é utilizado em conjunto com arrays, que é algo que ainda não foi passado, mas
+    // usaremos de maneira breve para exemplo
+    val lista = listOf("a", "b", "c", "d")
+    for (item in lista) {
+        println(item)
+    }
+
+    // Esse código vai fazer com que a "variavel" item, pegasse um por um cada dado inserido na variavel
+    // lista
 }
