3279a203b8649af824fdcb4523f546f27526b8f0
Luiz Henrique <luizbtc13@gmail.com>
2023-03-26 10:02:33 -0300

    Conditional Structures, a little more

--- src/main/kotlin/3.InstrucoesCondicionais.kt
+++ src/main/kotlin/3.InstrucoesCondicionais.kt
@@ -33,4 +33,25 @@ fun main() {
 
     // Esse código vai fazer com que a "variavel" item, pegasse um por um cada dado inserido na variavel
     // lista
+
+    // Você também pode trabalhar com duas ou mais variaveis dentro das estruturas condicionais, por
+    // exemplo:
+    val name = "Jorge"
+    val yearsInvesting = 10
+
+    if (name == "Jorge" && yearsInvesting > 8) {
+        println("Jorge é um investidor de longo prazo")
+    } else if (name == "Jorge" && (yearsInvesting > 3 && yearsInvesting <= 8)) {
+        println("Jorge é um investidor de médio prazo")
+    } else if (name == "Jorge" && yearsInvesting <= 3) {
+        println("Jorge é um investidor de curto prazo")
+    } else {
+        println("Jorge não é um investidor")
+    }
+
+    // Quando eu utilizo o &&, ele repesenta a letra E(AND), quer dizer que ambas as afirmações(que o nome
+    // do rapaz é jorge e o número de anos investidos), tenham que ser corretos, para que o código específico
+    // seja verificado, além do &&, tem outros simbolos que podem ser usados que significam coisas diferentes
+    // estudar sobre tabela verdade para esclarecer sobre o assunto
+
 }
